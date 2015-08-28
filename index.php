521a50c147fe4d7e0d1817a671a113faab729521
Nick Hargreaves <hargreaves1234@gmail.com>
2015-08-28 12:09:43 +0300

    properly add plugin information

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
 <?php
-/**
- * Plugin Name: WP Citizen Reporter
- * Plugin URI: http://github.com/codeforafrica/WPCitizenReporter
- * Description: A Wordpress plugin with enhancements for use with the CitizenReporter application
- * Version: 1.0.0
- * Author: Nick Hargreaves
- * Author URI: http://nickhargreaves.com
- * License: GPL2
- */
--- plugin-name.php
+++ plugin-name.php
@@ -8,21 +8,13 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
- * @since             1.0.0
- * @package           Plugin_Name
- *
- * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
- * Domain Path:       /languages
+ * Plugin Name: WP Citizen Reporter
+ * Plugin URI: http://github.com/codeforafrica/WPCitizenReporter
+ * Description: A Wordpress plugin with enhancements for use with the CitizenReporter application
+ * Version: 1.0.0
+ * Author: Nick Hargreaves
+ * Author URI: http://nickhargreaves.com
+ * License: GPL2
  */
 
 // If this file is called directly, abort.
