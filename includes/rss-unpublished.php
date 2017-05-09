98735b2538e0afc01415b36b0177c9a24ea53b0d
Phillip Ahereza <pahereza@gmail.com>
2017-05-09 11:17:05 +0300

    rss feed type

--- includes/rss-unpublished.php
+++ includes/rss-unpublished.php
@@ -13,10 +13,7 @@ $args = array(
 
 $posts = query_posts($args);
 
-// Setting up content type and charset headers
-header('Content-Type: '.feed_content_type('rss-http').';charset='.get_option('blog_charset'), true);
-
-// Setting up valid XML encoding
+header('Content-Type: '.feed_content_type('rss-http').'; charset='.get_option('blog_charset'), true);
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
 ?>
 
@@ -29,54 +26,30 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
      xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
      xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
     <?php do_action('rss2_ns'); ?>>
-    <!-- Declaring channel with articles data -->
-    <?php $dateTimeFormat = 'D, M d Y H:i:s'; ?>
     <channel>
         <title><?php bloginfo_rss('name'); ?> - Feed</title>
+        <atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
         <link><?php bloginfo_rss('url') ?></link>
         <description><?php bloginfo_rss('description') ?></description>
-        <lastBuildDate><?php echo mysql2date($dateTimeFormat, get_lastpostmodified(), false); ?></lastBuildDate>
+        <lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></lastBuildDate>
         <language><?php echo get_option('rss_language'); ?></language>
-        <sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'daily' ); ?></sy:updatePeriod>
+        <sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
         <sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
         <?php do_action('rss2_head'); ?>
-        <?php while (have_posts()) : the_post(); ?>
+        <?php while(have_posts()) : the_post(); ?>
             <item>
-                <title><?php the_title_rss() ?></title>
-                <link><?php the_permalink_rss() ?></link>
-                <?php if ( get_comments_number() || comments_open() ) : ?>
-                    <comments><?php comments_link_feed(); ?></comments>
-                <?php endif; ?>
+                <title><?php the_title_rss(); ?></title>
+                <link><?php the_permalink_rss(); ?></link>
                 <pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
-                <dc:creator><![CDATA[<?php the_author() ?>]]></dc:creator>
-                <?php the_category_rss('rss2') ?>
-
+                <dc:creator><?php the_author(); ?></dc:creator>
                 <guid isPermaLink="false"><?php the_guid(); ?></guid>
-                <?php if (get_option('rss_use_excerpt')) : ?>
-                    <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
-                <?php else : ?>
-                    <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
-                    <?php $content = get_the_content_feed('rss2'); ?>
-                    <?php if ( strlen( $content ) > 0 ) : ?>
-                        <content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
-                    <?php else : ?>
-                        <content:encoded><![CDATA[<?php the_excerpt_rss(); ?>]]></content:encoded>
-                    <?php endif; ?>
-                <?php endif; ?>
-                <?php if ( get_comments_number() || comments_open() ) : ?>
-                    <wfw:commentRss><?php echo esc_url( get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
-                    <slash:comments><?php echo get_comments_number(); ?></slash:comments>
-                <?php endif; ?>
+                <description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
+                <content:encoded><![CDATA[<?php the_excerpt_rss() ?>]]></content:encoded>
                 <?php rss_enclosure(); ?>
-                <?php
-                /**
-                 * Fires at the end of each RSS2 feed item.
-                 *
-                 * @since 2.0.0
-                 */
-                do_action( 'rss2_item' );
-                ?>
+                <?php do_action('rss2_item'); ?>
             </item>
         <?php endwhile; ?>
     </channel>
 </rss>
+
+
